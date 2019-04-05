ad6a9c68c295b0e7230f6539b727b3362bcc4d5f
belafu <gyupanquisalvatierra@gmail.com>
2019-04-05 12:10:44 -0300

    Subo primera Clase de PHP

---
+++ clase1/index.php
@@ -0,0 +1,34 @@
+<?php
+echo "<p> PHP CLASE 1 </p>";
+
+$num=44;
+echo $num ."<br>";
+$numDecimal = 12.4;
+echo $numDecimal."<br>";
+$cadenaSimple = 'qggd';
+echo $cadenaSimple."<br>";
+$cadenaDoble = "hola";
+echo $cadenaDoble."<br>";
+echo"<br>";
+$num = "hola mundo";
+echo $num."<br>";
+
+$cadenaSimple = 12.11;
+echo $cadenaSimple."<br>";
+
+$num = "34";
+echo $num;
+echo"<br>";
+echo"<br>";
+$uno ="Tres ";
+$dos="tristes ";
+$tres="tigres ";
+$cuatro="tragan ";
+$cinco="trigo ";
+$seis="en ";
+$siete="un ";
+$ocho="trigal";
+echo $uno.$dos.$tres.$cuatro.$cinco.$seis.$siete.$ocho;
+echo"<br>";
+echo "OCho "."Alegres ".$tres."Comen ".$cinco.$seis.$siete.$ocho;
+?>
