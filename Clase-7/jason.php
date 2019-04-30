dd26daa7b6d754864a01b7cc00f015d01b5aec74
LizaTheAnnihilator <gyupanquisalvatierra@gmail.com>
2019-04-30 18:38:38 -0300

    Hago avanses de ultimas clases

--- Clase-7/jason.php
+++ Clase-7/jason.php
@@ -1,26 +1,38 @@
 <?php
 
 $a =array ( 'a'=>1, 'b'=>2, 'c'=>'Yo <3 JSON');
-var_dump($a);
-echo "<hr>";
+var_dump($a);echo "<br>";
 //Utilizando json_encode, convertir el array en un json y asignarlo a $a.
 $a=json_encode($a);
-var_dump($a);
-echo "<hr>";
 //Hacer echo de la variable $a.
+echo "Array en formato json: ".$a;echo "<br>";//ACA FUNCIONA ELN ECHO POR QUE YA NO ES UN ARRAY SINO UN STRING
 //Crear una nueva variable $b que contenga el json_decode de la variable $a.
-//Hacer echo de $b.
+$b = json_decode($a,true);//JSON DECODIFICADO A ARRAY ASOCIATIVO
+var_dump($b);echo "<br>";
 //Imprimir la frase “Yo <3 JSON | 1 | 2 |” utilizando los datos de la variable $b
-$auto =json_decode('{"color":"rojo",
-                     "marca":"yaris",
-                     "modelo":"citroe"}',true);
-var_dump($auto);
+echo $b['c']." | ".$b['a']." | ".$b['b']." |<br>";
 echo "<hr>";
 
-$archAuto = file_get_contents("archivos.json");
-var_dump($archAuto);
-$archAuto = json_decode($archAuto,true);
-var_dump($archAuto);
-
-
+//Leer el archivo e imprimir un checkbox por cada categoría, capaz de ser enviado como array en un formulario. El value de cada checkbox será el id y el label mostrado a su derecha será el nombre proveniente del json como vemos en el en el array.
+$categorias = file_get_contents("categorias.json");
+$categorias = json_decode($categorias,true);
+var_dump($categorias);
+echo "<hr>";
  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>JSON</title>
+  </head>
+  <body>
+    <form class="" action="jason.php" method="post">
+        <?php foreach ($categorias["categorias"] as $key => $value): ?>
+            <input type="checkbox" name="" value=<?=  $value["id"] ?>> <?=  $value["nombre"] ?>
+
+        <?php endforeach; ?>
+        <br>
+        <button type="button" name="button">Enviar</button>
+    </form>
+  </body>
+</html>
--- Clase-7/register.php
+++ Clase-7/register.php
@@ -3,12 +3,37 @@
 $nombre="";
 $email='';
 $nombreUsuario='';
+$contra='';
 
 if ($_POST) {
   $nombre = $_POST["name"];
   $email = $_POST["email"];
   $nombreUsuario = $_POST["username"];
+  $contra = $_POST["password"];
+  echo "Tu contraseña es: ".$contra."<br>";
+  $nuevoUsuario = armarUsuario();
+  var_dump($nuevoUsuario);
+  coinciden($contra,$nuevoUsuario["contraseña"]);
 }
+
+function armarUsuario(){
+  return  [
+    "Nombre" => $_POST["name"],
+    "Email" =>  $_POST["email"],
+    "Usuario" => $_POST["username"],
+    "contraseña" => password_hash($_POST["password"],PASSWORD_DEFAULT)
+  ];
+}
+function coinciden($contraseña,$contraHasheada){
+  if (password_verify($contraseña,$contraHasheada)) {
+    echo "coindiden las contraseñas";
+  }
+  else {
+    echo "NO coinciden";
+  }
+}
+
+
 
 
 ?>
@@ -26,23 +51,23 @@ if ($_POST) {
                 <legend>Registrate</legend>
                 <div class='container'>
                     <label for='name' >Nombre completo: </label><br/>
-                    <input type='text' name='name' id='name' value='' maxlength="50" /><br/>
+                    <input type='text' name='name' id='name' value='<?= $nombre ?>' maxlength="50" /><br/>
                     <span id='register_name_errorloc' class='error'></span>
                 </div>
                 <div class='container'>
                     <label for='email' >Email:</label><br/>
-                    <input type='text' name='email' id='email' value='' maxlength="50" /><br/>
+                    <input type='text' name='email' id='email' value='<?= $email ?>' maxlength="50" /><br/>
                     <span id='register_email_errorloc' class='error'></span>
                 </div>
                 <div class='container'>
                     <label for='username' >Nombre de usuario*:</label><br/>
-                    <input type='text' name='username' id='username' value='' maxlength="50" /><br/>
+                    <input type='text' name='username' id='username' value='<?= $nombreUsuario ?>' maxlength="50" /><br/>
                     <span id='register_username_errorloc' class='error'></span>
                 </div>
                 <div class='container' style='height:80px;'>
                     <label for='password' >Contaseña*:</label><br/>
                     <div class='pwdwidgetdiv' id='thepwddiv' ></div>
-                    <input type='password' name='password' id='password' maxlength="50" />
+                    <input type='password' name='password' id='password' maxlength="50" required/>
                     <div id='register_password_errorloc' class='error' style='clear:both'></div>
                 </div>
 
